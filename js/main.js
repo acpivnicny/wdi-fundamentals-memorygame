057b47da8f0ddaa965c84001a5d019fb61ff73f2
Pivnicny <alexis.c.pivnicny@disney.com>
2018-01-06 22:50:52 -0800

    game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
-
+// create array of cards
+var cards = ["queen", "queen", "king", "king"];
+// create array for cards in play
+var cardsInPlay = []
+// user flips card one
+var cardOne = cards[0];
+// card one in play
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+//user flips card two
+var cardTwo = cards[2];
+//card two in play
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+// how many cards in cards in play
+if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+
+
+}
+
+}
