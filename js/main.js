a0ad6913010b1dd10006070f39f9a52f30755cc6
Pivnicny <alexis.c.pivnicny@disney.com>
2018-01-10 20:45:53 -0800

    images

--- js/main.js
+++ js/main.js
@@ -1,27 +1,53 @@
 
-// create array of cards
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    card: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    card: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    card: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    card: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+];
 // create array for cards in play
-var cardsInPlay = []
-// user flips card one
-var cardOne = cards[0];
-// card one in play
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-//user flips card two
-var cardTwo = cards[2];
-//card two in play
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+var cardsInPlay = [];
 
-// how many cards in cards in play
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
 if (cardsInPlay[0] === cardsInPlay[1]) {
   alert("You found a match!");
 } else {
   alert("Sorry, try again.");
-
-
 }
+};
 
-}
+var flipCard = function (cardId) {
+  console.log("User flipped " + " " + cards[cardId].card);
+  cardsInPlay.push(cards[cardId].card);
+
+  cardsInPlay.push(cards[cardId].card);
+
+  console.log(cards[cardId].cardImage)
+
+  console.log(cards[cardId].suit)
+
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+    cardsInPlay = [];
+  }
+};
+
+
+flipCard(0);
+flipCard(2);
