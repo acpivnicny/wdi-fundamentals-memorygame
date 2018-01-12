3eb0552ae9f6b72f0d96f246bfc90340443b71b0
Pivnicny <alexis.c.pivnicny@disney.com>
2018-01-11 19:41:29 -0800

    final dom

--- js/main.js
+++ js/main.js
@@ -32,22 +32,29 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 }
 };
 
-var flipCard = function (cardId) {
-  console.log("User flipped " + " " + cards[cardId].card);
-  cardsInPlay.push(cards[cardId].card);
+var flipCard = function () {
+  cardId = this.getAttribute('data-id');
+  console.log(cardId);
 
   cardsInPlay.push(cards[cardId].card);
+this.setAttribute('src', cards[cardId].cardImage);
+if (cardsInPlay.length === 2) {
+  checkForMatch();
+  cardsInPlay = [];
 
-  console.log(cards[cardId].cardImage)
-
-  console.log(cards[cardId].suit)
-
-  if (cardsInPlay.length === 2) {
-    checkForMatch();
-    cardsInPlay = [];
   }
 };
 
+  var createBoard = function () {
+    for (var i = 0; i < cards.length; i++) {
 
-flipCard(0);
-flipCard(2);
+      var cardElement = document.createElement('img');
+      cardElement.setAttribute('src', 'images/back.png');
+      cardElement.setAttribute('data-id', i);
+      cardElement.addEventListener('click', flipCard);
+
+      document.getElementById('game-board').appendChild(cardElement);
+    }
+
+  }
+  createBoard();
